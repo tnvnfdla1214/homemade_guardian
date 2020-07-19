8b356c536fd0a2bc5bcd5a9caed813b57e572f0e
Aduba <likppi100@naver.com>
2020-07-20 03:56:37 +0900

    바텀바 추가한 것

---
+++ app/src/main/java/com/example/homemade_guardian_beta/fragment/WritePostFragment.java
@@ -0,0 +1,40 @@
+package com.example.homemade_guardian_beta.fragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.homemade_guardian_beta.R;
+
+public class WritePostFragment extends Fragment {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_writepost, container, false);
+
+        return view;
+    }
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+    }
+}

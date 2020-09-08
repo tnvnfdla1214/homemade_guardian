2980da6b63877fa98a3162ea53d01007921f3418
Min Gyu <tnvnfdla12@gmail.com>
2020-09-08 10:06:59 +0900

    챗이랑 합침

--- app/src/main/java/com/example/homemade_guardian_beta/chat/activity/ChatActivity.java
+++ app/src/main/java/com/example/homemade_guardian_beta/chat/activity/ChatActivity.java
@@ -1,8 +1,10 @@
 package com.example.homemade_guardian_beta.chat.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
@@ -12,6 +14,7 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 
+import com.example.homemade_guardian_beta.Main.activity.MainActivity;
 import com.example.homemade_guardian_beta.R;
 import com.example.homemade_guardian_beta.chat.fragment.ChatFragment;
 import com.example.homemade_guardian_beta.chat.fragment.GroupUserFragment;
@@ -22,6 +25,9 @@ public class ChatActivity extends AppCompatActivity {
     private DrawerLayout drawerLayout;
     private ChatFragment chatFragment;
     private GroupUserFragment groupUserFragment = null;
+    String To_User_Uid;
+    String ChatRoomListModel_RoomUid;
+    String ChatRoomListModel_Title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,14 +40,13 @@ public class ChatActivity extends AppCompatActivity {
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setHomeButtonEnabled(true);
 
-        String To_User_Uid = getIntent().getStringExtra("To_User_Uid");     //MyInfoFragment에서 받아옴 ,PostActivity에서 받아옴
-        final String ChatRoomListModel_RoomUid = getIntent().getStringExtra("RoomUid");     //ChatRoomListFragment에서 받아옴
-        String ChatRoomListModel_Title = getIntent().getStringExtra("ChatRoomListModel_Title"); //ChatRoomListFragment에서 받아옴
+        To_User_Uid = getIntent().getStringExtra("To_User_Uid");     //MyInfoFragment에서 받아옴 ,PostActivity에서 받아옴
+        ChatRoomListModel_RoomUid = getIntent().getStringExtra("RoomUid");     //ChatRoomListFragment에서 받아옴
+        ChatRoomListModel_Title = getIntent().getStringExtra("ChatRoomListModel_Title"); //ChatRoomListFragment에서 받아옴
 
         if (ChatRoomListModel_Title!=null) { actionBar.setTitle(ChatRoomListModel_Title); }
-
-        // left drawer
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        /*
         findViewById(R.id.rightMenuBtn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -59,6 +64,8 @@ public class ChatActivity extends AppCompatActivity {
                 }
             }
         });
+
+         */
         // chatting area
         chatFragment = ChatFragment.getInstance(To_User_Uid, ChatRoomListModel_RoomUid);
         getSupportFragmentManager()
@@ -67,20 +74,31 @@ public class ChatActivity extends AppCompatActivity {
                 .commit();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
-            case android.R.id.home:
-                onBackPressed();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
 
     @Override
     public void onBackPressed() {
         chatFragment.backPressed();
         finish();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {                                                         // part19 : 게시물 안에서의 수정 삭제 (58')
+        getMenuInflater().inflate(R.menu.chat_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    //메뉴 안에 있는 버튼들의 이벤트
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.Chat_Delete_Button:
+                //이거 3번째 프레그먼트로 넘어가야 함
+                Log.d("태그","ChatRoomListModel_RoomUid : " + ChatRoomListModel_RoomUid);
+                Intent Intent_MainActivity = new Intent(ChatActivity.this, MainActivity.class);
+                startActivity(Intent_MainActivity);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
